7af4dd6de0a171100cf4004d90ad55881fa2b950
DanKos22 <G00397054@atu.ie>
2023-10-03 16:56:49 +0100

    added greetByName method

--- src/main/java/ie/atu/hello/HelloController.java
+++ src/main/java/ie/atu/hello/HelloController.java
@@ -1,5 +1,6 @@
 package ie.atu.hello;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -8,5 +9,12 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello, World!";
+    }
+
+
+
+    @GetMapping("/greet/{name}")
+    public String greetByName(@PathVariable String name) {
+        return "Hello" + name;
     }
 }
