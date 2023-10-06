350555c172f7a5502cc4dca76ca34b23cda33c23
DanKos22 <G00397054@atu.ie>
2023-10-06 14:02:01 +0100

    Modified code

--- src/main/java/ie/atu/hello/CalculatorController.java
+++ src/main/java/ie/atu/hello/CalculatorController.java
@@ -6,21 +6,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
 @RestController
 public class CalculatorController {
-
-
     number total = new number();
-    int num1, num2;
-    String operation;  //= getTotal();
 
-
-    @GetMapping("/calculate")
-    public String calculate(@RequestParam int num1, @RequestParam int num2, @RequestParam String operation) {
-        //int total = num1 + num2;
-        return "operation: " + "\tadd" + "\ttotal:" + total.getTotal();
+    @GetMapping("/calculate/{num1}/{num2}/{operation}")
+    public String calculate(@PathVariable int num1, @PathVariable int num2, @PathVariable String operation) {
+        return "operation: " + "\tdivision" + "\ttotal:" + total.getDivision();
     }
-
 }
--- src/main/java/ie/atu/hello/number.java
+++ src/main/java/ie/atu/hello/number.java
@@ -1,36 +1,44 @@
 package ie.atu.hello;
 
 public class number {
-    int num1 = 6;
-    int num2 = 2;
-    int total = num1 + num2;
-    int operation = num1 + num2;
-
+    int num1 = 6, num2 = 2, num3 = 10, num4 = 5, num5 = 4, num6 = 3, num7 = 8, num8 = 2;
+    int add = num1 + num2;
+    int subtract = num3 - num4;
+    int multiply = num5 * num6;
+    float division = num7 / num8;
 
     public number() {
     }
 
-    /*public number(double operation) {
-        this.operation = operation;
-    }*/
-    public int add() {
-        int total = num1 + num2;
-        return total;
+    public int getAdd() {
+        return add;
     }
 
-    public int getOperation() {
-        return operation;
+    public void setAdd(int add) {
+        this.add = add;
     }
 
-    public void setOperation(int operation) {
-        this.operation = operation;
+    public int getSubtract() {
+        return subtract;
     }
 
-    public int getTotal() {
-        return total;
+    public void setSubtract(int subtract) {
+        this.subtract = subtract;
     }
 
-    public void setTotal(int total) {
-        this.total = total;
+    public int getMultiply() {
+        return multiply;
+    }
+
+    public void setMultiply(int multiply) {
+        this.multiply = multiply;
+    }
+
+    public float getDivision() {
+        return division;
+    }
+
+    public void setDivision(float division) {
+        this.division = division;
     }
 }
